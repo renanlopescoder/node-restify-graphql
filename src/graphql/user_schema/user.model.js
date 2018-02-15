70dad4d2f09f810c811cd6b891d7e4a4a3bc4433
Renan Lopes <renanlopescoder@gmail.com>
2018-02-15 21:55:48 -0200

    Feature: Server for production with PM2

--- src/graphql/user_schema/user.model.js
+++ src/graphql/user_schema/user.model.js
@@ -1,14 +1,31 @@
-const mongoose = require('mongoose');
+import mongoose, { Schema } from 'mongoose';
 
-const schema = mongoose.Schema({
-			name: {
-				type: String,
-				required: false,
-			},
-			email : {
-				type: String,
-				required: false,
-			},
-	}, {timestamps: true});
+const UserSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: false,
+		},
+		password : {
+			type: String,
+			required: true,
+		},
+		email : {
+			type: String,
+			required: true,
+		},
+		photo : {
+			type: String,
+			required: false,
+		},
+		nickname: {
+			type: String,
+			required: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
-export default mongoose.model('User', schema);
+export default mongoose.model('User', UserSchema);
